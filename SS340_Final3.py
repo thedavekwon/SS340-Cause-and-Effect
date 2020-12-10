b5b5d970913914eb4b874d177c5f0162ac8c73a9
friedmanjzachary <friedmanjzachary@gmail.com>
2020-12-10 12:35:16 -0500

    processed data

---
+++ SS340_Final3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import time
+from geopy.geocoders import Nominatim
+import geopandas
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from geopy.distance import distance, lonlat
+from scipy import stats
+import statsmodels.api as sm
+
+
+df = pd.read_csv('processed_net.csv')
+
+print(df['Race'].unique())
+
+df['Black'] = 0
+df['Asian'] = 0
+df.loc[df['Race'] == 'Black or African American', 'Black'] = 1
+df.loc[df['Race'] == 'Asian or Pacific Islander', 'Asian'] = 1
+
+
+print(df.describe())
+X = df[['min_dist','Sex', 'Black', 'Asian']]
+X = sm.add_constant(X)
+est = sm.OLS(df['Crude Rate'], X.astype(float))
+est = est.fit(cov_type='HC1')
+print(est.summary())
+
+
+
+
